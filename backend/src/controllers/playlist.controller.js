dd55d610232014fbdc350a7af117d6ec11080c63
chaitanya <dev.chaitanyasonawane@gmail.com>
2025-12-24 17:04:35 +0530

    playlist controllers done

--- backend/src/controllers/playlist.controller.js
+++ backend/src/controllers/playlist.controller.js
@@ -1,24 +1,166 @@
+import {db} from "../libs/db.js"
 
-export const getAllPlaylist = (req, res) => {
+export const getAllPlaylist = async (req, res) => {
+    try {
+        const playlists = await db.playlist.findMany({
+            where : {
+                userId : req.user.id
+            },
+            include : {
+                problems : {
+                    include : {
+                        problem : true
+                    }
+                }
+            }
+        })
 
+        return res.status(200).json({
+            success : true,
+            message : "playlist fetched",
+            playlists
+        })
+    } catch (error) {
+        return res.json({
+            message : "server error"
+        })
+    }
 }
 
-export const getPlayListDetails = (req, res) => {
+export const getPlayListDetails = async (req, res) => {
+    const {playlisId} = req.params
 
+    try {
+        const playlist = await db.playlist.findMany({
+            where : {
+                userId : req.user.id,
+                id : playlisId
+            },
+            include : {
+                problems : {
+                    include : {
+                        problem : true
+                    }
+                }
+            }
+        })
+
+        if(!playlist) {
+            return res.json({
+                message : "playlist not found"
+            })
+        }
+
+        return res.json({
+            message : "playlist fetched",
+            playlist
+        })
+    } catch (error) {
+        return res.json({
+            message : "server error"
+        })
+    }
 }
 
-export const createPlayList = (req, res) => {
+export const createPlayList = async (req, res) => {
+    const {name, description} = req.body
+    const userId = req.user.id
 
+    try {
+        const playlist = await db.playlist.create({
+            data : {
+                name,
+                description,
+                userId
+            }
+        })
+
+        return res.status(200).json({
+            success : true,
+            message : "playlist created"
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message : "server error"
+        })
+    }
 }
 
-export const addProblemToPlayList = (req, res) => {
+export const addProblemToPlayList = async (req, res) => {
+    const {playlisId} = req.params
+    const problemIds = req.body
 
+    try {
+        if(!Array.isArray(problemIds) || problemIds.length === 0){
+            return res.json({
+                message : "invalid or missing problemsid"
+            })
+        }
+
+        const problemInPlaylist = await db.playlist.createMany({
+            data : problemIds.map((problemId) => ({
+                playlisId, 
+                problemId
+            }))
+        })
+
+        return res.json({
+            message : "problem added in playlist",
+            problemInPlaylist
+        })
+    } catch (error) {
+        return res.json({
+            message : " internal server error"
+        })
+    }
 }
 
-export const deletePlayList = (req, res) => {
+export const deletePlayList = async (req, res) => {
+    const {playlisId} = req.params
+    try {
+        const deletedPlaylist = await db.playlist.delete({
+            where : {
+                id : playlisId
+            }
+        })
 
+        return res.json({
+            message : "playlist deleted",
+            deletedPlaylist
+        })
+    } catch (error) {
+        return res.json({
+            message : "server error"
+        })
+    }
 }
 
-export const removeProblemFromPlayList = (req, res) => {
-    
+export const removeProblemFromPlayList = async (req, res) => {
+    const {playlisId} = req.params
+    const problemIds = req.body
+
+    try {
+        if (!Array.isArray(problemIds) || problemIds.length === 0) {
+          return res.json({
+            message: "invalid or missing problemsid",
+          });
+        }
+
+        const deletetedProblem = await db.playlist.delete({
+            where : {
+                playlisId,
+                problemId : {
+                    in : problemIds
+                }
+            }
+        })
+
+        return res.json({
+            message : "deleted from playlist"
+        })
+    } catch (error) {
+        return res.json({
+            message : "server error"
+        })
+    }
 }
